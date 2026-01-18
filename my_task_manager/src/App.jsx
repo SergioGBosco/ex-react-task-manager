0da010edf0d78f7d265f645c045001e436ef9b6e
SergioGBosco <sergiogibosco@gmail.com>
2026-01-18 20:11:05 +0100

    Milestone 1 - setup Layout

---
+++ my_task_manager/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"
+import TaskList from "./pages/TaskList"
+import AddTask from "./pages/AddTask"
+function App() {
+
+
+  return (
+    <BrowserRouter>
+
+      <nav>
+        <NavLink to="/">Lista Task</NavLink>
+        <NavLink to="/add">Aggiungi Task</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<TaskList />} />
+        <Route path="/add" element={<AddTask />} />
+      </Routes>
+
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ my_task_manager/src/pages/TaskList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div>
+      <h1>Questa Pagina mostra la lista delle task</h1>
+    </div>
+  )
+}
+
+export default TaskList;
