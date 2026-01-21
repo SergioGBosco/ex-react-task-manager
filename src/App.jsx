2e680ac01a3c92a75d7416a6c00129149eff86aa
SergioGBosco <sergiogibosco@gmail.com>
2026-01-21 17:55:26 +0100

    Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"
 import TaskList from "./pages/TaskList"
 import AddTask from "./pages/AddTask"
+import { GlobalProvider } from "./context/GlobalContext"
 function App() {
 
 
   return (
-    <BrowserRouter>
+    <GlobalProvider>
+      <BrowserRouter>
 
-      <nav>
-        <NavLink to="/">Lista Task</NavLink>
-        <NavLink to="/add">Aggiungi Task</NavLink>
-      </nav>
+        <nav>
+          <NavLink to="/">Lista Task</NavLink>
+          <NavLink to="/add">Aggiungi Task</NavLink>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<TaskList />} />
-        <Route path="/add" element={<AddTask />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<TaskList />} />
+          <Route path="/add" element={<AddTask />} />
+        </Routes>
 
-    </BrowserRouter>
+      </BrowserRouter>
+    </GlobalProvider>
   )
 }
 
--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalContext';
+
 
 const TaskList = () => {
+
+  const { tasks } = useContext(GlobalContext);
+  console.log(`tasks:`, tasks)
+
   return (
     <div>
       <h1>Questa Pagina mostra la lista delle task</h1>
