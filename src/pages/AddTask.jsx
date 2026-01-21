4b2ffb26eccfa87c530195ca04d504623e274b63
SergioGBosco <sergiogibosco@gmail.com>
2026-01-21 23:09:06 +0100

    Mileston 6 - use form for add task

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+
+import { useState, useRef } from 'react';
+
+const symbols = "!@#$%^&*()-_=+[]{}|;:'\\,.<>?/`~";
 
 const AddTask = () => {
+
+  const [taskTitle, setTaskTitle] = useState("");
+  const descriptionRef = useRef();
+  const statusRef = useRef()
+
+  const taskTitleError = useMemo(() => {
+    if (!taskTitle.trim()) return "Inserisci un nome per la task valido"
+    if ([...taskTitle].some(carattere => symbols.includes(carattere)))
+      return "non è possibile inserire simboli"
+    return "";
+  }, [taskTitle]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (taskTitleError)
+      return;
+    const newTask = {
+      title: taskTitle.trim(),
+      description: descriptionRef.current.value,
+      status: statusRef.current.value
+    }
+    console.log("Nuova Task:", newTask)
+  }
+
   return (
     <div>
       <h1>Pagina Aggiunta Task</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nome Task:
+          <input type="text"
+            value={taskTitle}
+            onChange={e => setTaskTitle(e.target.value)} />
+        </label>
+        {taskTitleError &&
+          <p style={{ color: `red` }}>{taskTitleError}</p>
+        }
+        <label>
+          Descrizione:
+          <textarea ref={descriptionRef} />
+        </label>
+        <label>
+          Stato:
+          <select ref={statusRef}>
+            <option value="To do">To do</option>
+            <option value="Doing">Doing</option>
+            <option value="Done">Done</option>
+          </select>
+        </label>
+        <button type='submit' disabled={taskTitleError}>Aggiungi Task</button>
+      </form>
+
+
     </div>
   )
 }
